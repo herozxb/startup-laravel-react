a55362ed1f230fa9d9613cf7a536fbdbf2c014bc
herozxb <herozxb@gmail.com>
2021-09-13 16:45:08 +0800

    bug to accept the video stream

--- resources/js/Example.js
+++ resources/js/Example.js
@@ -196,19 +196,19 @@ const Example = (props) => {
                         					</Button>
                         				      </CopyToClipboard>
                         				    </Grid>
-                        				    <Grid item xs={12} md={6} className={classes.padding}>
-                        				      <Typography gutterBottom variant="h6">视频通话</Typography>
-                        				      <TextField label="填写对方的电话ID号码" value={idToCall} onChange={(e) => setIdToCall(e.target.value)} fullWidth />
-                        				      {callAccepted && !callEnded ? (
-                        					<Button variant="contained" color="secondary" startIcon={<PhoneDisabled fontSize="large" />} fullWidth onClick={leaveCall} className={classes.margin}>
-                        					  挂掉电话
-                        					</Button>
-                        				      ) : (
-                        					<Button variant="contained" color="primary" startIcon={<Phone fontSize="large" />} fullWidth onClick={() => callUser(idToCall)} className={classes.margin}>
-                        					  视频通话
-                        					</Button>
-                        				      )}
-                        				    </Grid>
+            <Grid item xs={12} md={6} className={classes.padding}>
+              <Typography gutterBottom variant="h6">Make a call</Typography>
+              <TextField label="ID to call" value={idToCall} onChange={(e) => setIdToCall(e.target.value)} fullWidth />
+              {callAccepted && !callEnded ? (
+                <Button variant="contained" color="secondary" startIcon={<PhoneDisabled fontSize="large" />} fullWidth onClick={leaveCall} className={classes.margin}>
+                  Hang Up
+                </Button>
+              ) : (
+                <Button variant="contained" color="primary" startIcon={<Phone fontSize="large" />} fullWidth onClick={() => callUser(idToCall)} className={classes.margin}>
+                  Call
+                </Button>
+              )}
+            </Grid>
                         				  </Grid>
 
                         				</form>
