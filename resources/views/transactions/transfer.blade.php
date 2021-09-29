0179d0051c7908d4b37c8e02498fab59c010118b
herozxb <herozxb@gmail.com>
2021-09-29 17:52:50 +0800

    second menu bug fix

--- resources/views/transactions/transfer.blade.php
+++ resources/views/transactions/transfer.blade.php
@@ -14,6 +14,16 @@
 
 
 
+
+<link href="https://netdna.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.css" rel="stylesheet">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-star-rating/4.0.2/css/star-rating.min.css" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-star-rating/4.0.2/js/star-rating.min.js"></script>
+
+<!-- Content
+  ============================================= -->
+<div id="content" class="py-4">
+
 <!-- Secondary menu
   ============================================= -->
 <div class="bg-white">
@@ -27,14 +37,7 @@
 </div>
 <!-- Secondary menu end -->
 
-<link href="https://netdna.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.css" rel="stylesheet">
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-star-rating/4.0.2/css/star-rating.min.css" />
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-star-rating/4.0.2/js/star-rating.min.js"></script>
 
-<!-- Content
-  ============================================= -->
-<div id="content" class="py-4">
     <div class="container">
         <h2 class="font-weight-400 text-center mt-3">汇款</h2>
         <p class="text-4 text-center mb-4">在世界任何地方，发送汇款给任何人</p>
