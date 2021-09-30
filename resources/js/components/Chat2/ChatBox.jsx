60eae672bbd56d37bcca66bba1ddf557f9392b29
empzxb <54690707@qq.com>
2021-09-30 23:42:49 +0800

    fix the bubble

--- resources/js/components/Chat2/ChatBox.jsx
+++ resources/js/components/Chat2/ChatBox.jsx
@@ -65,7 +65,7 @@ const useStyles = makeStyles((theme) => ({
     borderRadius: "0 10px 10px 10px",
     boxShadow: "-3px 4px 4px 0px rgba(0,0,0,0.08)",
     marginTop: 8,
-    maxWidth: "30em",
+    maxWidth: "25em",
     color:theme.palette.primary.dark,
   },
   messageBubbleRight: {
@@ -86,7 +86,7 @@ const useStyles = makeStyles((theme) => ({
   },
   listItem: {
     display: "flex",
-    width: "100%",
+    width: "70%",
   },
   listItemRight: {
     flexDirection: "row-reverse",
