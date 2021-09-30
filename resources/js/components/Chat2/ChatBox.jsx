0899daf92bfdd75c9da4b0e3b43c3b41ccc77bbc
herozxb <herozxb@gmail.com>
2021-09-30 13:25:08 +0800

    fix the word mistake

--- resources/js/components/Chat2/ChatBox.jsx
+++ resources/js/components/Chat2/ChatBox.jsx
@@ -343,7 +343,7 @@ const ChatBox = (props) => {
                   <Modal.Footer>
                     <>
                       {(
-                        people_online&&<h3>对方不在线</h3>
+                        people_not_online&&<h3>对方不在线</h3>
                         )}
                     </>
                     <>
