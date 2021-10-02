7a6a28c96b957b1a8f9364f75f92ce91f642c845
empzxb <54690707@qq.com>
2021-10-02 09:46:15 +0800

    pass the props to conversation

--- resources/js/components/Chat2/Conversations.jsx
+++ resources/js/components/Chat2/Conversations.jsx
@@ -171,7 +171,7 @@ const Conversations = (props) => {
         </Modal.Header>
         <Modal.Body>
           <Grid item md={8}>
-              <ChatBox scope={props.scope} user={props.user} me_id={props.me_props} chat_user_id={user_id} setIdToCall_props_2={props.setIdToCall_props}  stream_props_2={props.stream_props} callUser_props_2={props.callUser_props}/>
+              <ChatBox scope={props.scope} user={props.user} me_id={props.me_props} chat_user_id={props.user_id} setIdToCall_props_2={props.setIdToCall_props}  stream_props_2={props.stream_props} callUser_props_2={props.callUser_props}/>
           </Grid>
         </Modal.Body>
         <Modal.Footer>
