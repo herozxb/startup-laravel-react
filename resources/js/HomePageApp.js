5df0d55eaa3c54ea0e67114f0a5b4f22cf876504
herozxb <herozxb@gmail.com>
2021-10-22 14:46:27 +0800

    add Row

--- resources/js/HomePageApp.js
+++ resources/js/HomePageApp.js
@@ -272,14 +272,18 @@ const HomePageApp = (props) => {
                 <Modal.Body>
                  <textarea type="text" value={texts} class="form-control" id="text-content" rows="3"  placeholder="世界 你好！" aria-label="" aria-describedby="basic-addon1" onChange={on_change_for_text} ></textarea>
                  <Row>
+                  <Row>
                    <input type="text" value={city} class="form-control" placeholder="地址" aria-label="" aria-describedby="basic-addon1" />
                     <Typography variant="caption" display="block" gutterBottom>
                       城市
                     </Typography>
+                  </Row>
+                  <Row>
                    <input type="text" value={salary} class="form-control" placeholder="薪资" aria-label="" aria-describedby="basic-addon1" />
                     <Typography variant="caption" display="block" gutterBottom>
                       每小时
                     </Typography>
+                  </Row>
                  </Row>
                 </Modal.Body>
                 <Modal.Footer>
