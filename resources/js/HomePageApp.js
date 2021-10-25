9208dd4fac501f9319479653ba81cafb1e67f102
herozxb <herozxb@gmail.com>
2021-10-25 15:59:43 +0800

    1.5 xs

--- resources/js/HomePageApp.js
+++ resources/js/HomePageApp.js
@@ -285,10 +285,10 @@ const HomePageApp = (props) => {
                       <Grid_Modal item xs={1} style={{display:'flex',justifyContent:'center',alignItems:'center'}} >
                         <span>城市</span>
                       </Grid_Modal>
-                      <Grid_Modal item xs={5}>
+                      <Grid_Modal item xs={4.5}>
                         <input type="text" value={salary} class="form-control" placeholder="薪资" aria-label="" aria-describedby="basic-addon1" />
                       </Grid_Modal>
-                      <Grid_Modal item xs={1} style={{display:'flex',justifyContent:'center',alignItems:'center'}} >
+                      <Grid_Modal item xs={1.5} style={{display:'flex',justifyContent:'center',alignItems:'center'}} >
                         <span>/小时</span>
                       </Grid_Modal>
                      </Grid_Modal>
