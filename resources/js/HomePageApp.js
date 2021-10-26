0733ec25c4a8e2a891bed67af8c87a20e9f62176
herozxb <herozxb@gmail.com>
2021-10-26 13:09:10 +0800

    ipnut /

--- resources/js/HomePageApp.js
+++ resources/js/HomePageApp.js
@@ -275,22 +275,15 @@ const HomePageApp = (props) => {
                   keyboard={false}
               >
                 <Modal.Header closeButton>
-<div class="btn-group">
-  <input
-    type="radio"
-    class="btn-check"
-    name="options"
-    id="option1"
-    autocomplete="off"
-    checked
-  />
-  <label class="btn btn-secondary" for="option1">Checked</label>
+<div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+  <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked />
+  <label class="btn btn-outline-primary" for="btnradio1">Radio 1</label>
 
-  <input type="radio" class="btn-check" name="options" id="option2" autocomplete="off" />
-  <label class="btn btn-secondary" for="option2">Radio</label>
+  <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off" />
+  <label class="btn btn-outline-primary" for="btnradio2">Radio 2</label>
 
-  <input type="radio" class="btn-check" name="options" id="option3" autocomplete="off" />
-  <label class="btn btn-secondary" for="option3">Radio</label>
+  <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off" />
+  <label class="btn btn-outline-primary" for="btnradio3">Radio 3</label>
 </div>
                 </Modal.Header>
                 <Modal.Body>
