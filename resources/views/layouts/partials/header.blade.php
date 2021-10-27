8dcd2759320e4d6c5f93f9c11fcc9d744b589c93
herozxb <herozxb@gmail.com>
2021-10-27 17:10:34 +0800

    add ,

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -18,7 +18,7 @@
                 <script>
                     console.log("===socket===");
                 
-                    var socket = io('https://120.53.220.237:5002'{
+                    var socket = io('https://120.53.220.237:5002',{
                         'reconnection': true,
                         'reconnectionDelay': 1000,
                         'reconnectionDelayMax' : 5000,
