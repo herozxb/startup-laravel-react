ac9b1cb0b496b3e5629a09998a20bc0c1a00dbf5
herozxb <herozxb@gmail.com>
2021-11-25 11:31:09 +0800

    id is changed

--- resources/js/HomePageApp.js
+++ resources/js/HomePageApp.js
@@ -488,7 +488,7 @@ const HomePageApp = (props) => {
                                           </Card.Text>}
 
                                         { user_login &&  <a href="/video" >
-                                              { post_search.user !== user_login.userId && <Button variant="primary" onClick={()=>{ on_jump_to_other_user(post_search.user)}} style={{ transform:' translate(55px,0px)'}}>联系他</Button>  
+                                              { post_search.user.$oid !== user_login.userId && <Button variant="primary" onClick={()=>{ on_jump_to_other_user(post_search.user.$oid)}} style={{ transform:' translate(55px,0px)'}}>联系他</Button>  
                                              }
                                           </a>  
                                         }
